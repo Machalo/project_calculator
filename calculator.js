393302d3b952f9be841bf3a2273a54bbe3451949
Machalo <150803687+Machalo@users.noreply.github.com>
2024-06-20 14:00:06 +0200

    Add . to the usable buttons and color and css mods

--- calculator.js
+++ calculator.js
@@ -23,6 +23,7 @@ let firstNum = "";
 let operator = "";
 let secondNum = "";
 let equal = false;
+let virgule = false;
 
 function operate (numOne, ope, numTwo){
     let calcul = 0;
@@ -39,8 +40,8 @@ function operate (numOne, ope, numTwo){
         calcul = div(numOne,numTwo);
     }
 
-    if (calcul.length >= 14 && !isNaN(calcul)) {
-        return calcul.toExponential(0);
+    if (calcul >= 999999999) {
+        return calcul.toExponential(7);
     }
     else{
         return calcul;
@@ -51,8 +52,6 @@ const buttons = document.querySelectorAll("button");
 const screenOp = document.getElementById("operation");
 const screenResult = document.getElementById("result");
 
-
-
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         if (button.id == "clear"){
@@ -60,10 +59,12 @@ buttons.forEach((button) => {
             operator = "";
             secondNum = "";
             equal = false;
+            virgule = false;
             screenResult.textContent = 0;
+            screenOp.textContent = 0;
         }
         if (operator.length == 0){
-            if (button.className == "number" && firstNum.length <= 10) {
+            if (button.className == "number" && firstNum.length <= 9) {
                 firstNum += button.id;
                 screenOp.textContent = firstNum;
                 screenResult.textContent = 0;
@@ -71,16 +72,28 @@ buttons.forEach((button) => {
             else if (button.className == "operator" && firstNum.length >= 1) {
                 operator += button.id;
                 screenOp.textContent = operator;
+                virgule = false;
+            }
+            else if (button.className == "dot" && firstNum.length >= 1 && virgule == false) {
+                firstNum += button.id;
+                screenOp.textContent = firstNum;
+                virgule = true;
             }
         }
         else if (operator.length >= 1){
-            if (button.className == "number" && secondNum.length <= 10) {
+            if (button.className == "number" && secondNum.length <= 9) {
                 secondNum += button.id;
                 screenOp.textContent = secondNum;
             }
             else if (button.className == "equal" && secondNum.length >= 1){
                 screenResult.textContent = operate(firstNum,operator,secondNum);
                 equal =true;
+                virgule = false;
+            }
+            else if (button.className == "dot" && secondNum.length >= 1 && virgule == false) {
+                secondNum += button.id;
+                screenOp.textContent = secondNum;
+                virgule = true;
             }
         }
         
